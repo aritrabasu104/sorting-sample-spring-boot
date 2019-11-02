1c22e5fa60629a30047cddf9cc7cdabd11b178e3
ARITRA BASU <aritrabasu104@gmail.com>
2019-11-02 23:39:20 +0530

    adding model to swagger

--- backend/src/main/java/hello/model/Employee.java
+++ backend/src/main/java/hello/model/Employee.java
@@ -7,7 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import io.swagger.annotations.ApiModel;
+
 @Entity
+@ApiModel(description = "Employee details. ")
 public class Employee {
 
 	@Id
